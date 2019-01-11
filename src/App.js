ebce8382dd8040476206c85a253753a96d60ed16
Oana-Poran <oanaporan@gmail.com>
2019-01-11 17:21:12 +0200

    Modal class  moved to own component file.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
 import React, { Component } from 'react'
-import ReactDom from 'react-dom'
+import Modal from './Modal'
 import './App.css';
 
-const modalRoot = document.getElementById('modal-root')
-
-class Modal extends Component {
-  constructor(props) {
-    super(props)
-    this.elem = document.createElement('div')
-  }
-
-  componentDidMount() {
-    modalRoot.appendChild(this.elem)
-  }
-  componentWillUnmount() {
-    modalRoot.removeChild(this.elem)
-  }
-
-  render() {
-    return ReactDom.createPortal(
-      this.props.children,
-      this.elem,
-    );
-  }
-}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {showModal : false};
-  }
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.onCloseModal)
+    // this.dialogElement = React.createRef();
   }
 
   onShowModal = () => {
-    this.setState({ showModal : true })
+    this.setState({ showModal : true });
   }
 
   onCloseModal = () => {
@@ -49,16 +24,16 @@ class App extends Component {
       <div className="App">
 
           <section>
-            <div id="sectionContent" aria-hidden="false">
+            <div id="sectionContent" aria-hidden={ this.state.showModal? 'true':'false' }>
                 <p onClick={this.onShowModal}> In as name to here them deny wise this. As rapid woody my he me which. Men but they fail shew just wish next put. Led all visitor musical calling nor her. Pretended concluded did repulsive education smallness yet yet described. Had country man his pressed shewing. No gate dare rose he. Eyes year if miss he as upon. 
                 </p>
               </div>
             {this.state.showModal ? (
                <div aria-label="Close" onClick={this.onCloseModal} id="dialogPlaceholder" role="dialog">
                <div>
-               <dialog tabIndex="-1">
-                   <Modal
-                        onRequestClose={this.onCloseModal} >
+               <dialog >
+                   <Modal method="dialog"
+                          onRequestClose={this.onCloseModal} >
                       <Child />
                    </Modal>        
                  </dialog>
@@ -76,8 +51,10 @@ class App extends Component {
 
 function Child() {
   return (
-    <div className="modal">
+    <div className="modal" >
+    {/* Here goes the modal content */}
       <p> Hi! I am your modal </p> 
+      <button> Accept </button>
     </div>
   )
 }
